c0305309de9f83dcdfd76e6f2caead3574dc26f2
William <nuages@gmail.com>
2025-08-29 20:07:08 -0400

    First commit of the fastapi news feed

---
+++ fastapi_npr_feed.py
@@ -0,0 +1,25 @@
+import feedparser
+from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    rss_url = "https://feeds.npr.org/1001/rss.xml"
+    d = feedparser.parse(rss_url)
+    return templates.TemplateResponse("index.html", {"feed": d, "request": request})
+
+
+@app.get("/bulma/", response_class=HTMLResponse)
+async def read_bulma(request: Request):
+    rss_url = "https://feeds.npr.org/1001/rss.xml"
+    d = feedparser.parse(rss_url)
+    return templates.TemplateResponse("bulma.html", {"feed": d, "request": request})
